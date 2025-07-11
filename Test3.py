a956da49e5b86c62122c7bab91e28797e3359a8c
amont <114557081+amont065@users.noreply.github.com>
2025-07-11 12:49:44 -0700

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -36,7 +36,6 @@ class TCGScraper:
     def init_driver(self):
         options = Options()
         options.headless = True
-        options.binary_location = '/usr/bin/firefox'
         driver = webdriver.Firefox(options=options)
         driver.maximize_window()
         return driver
@@ -204,7 +203,6 @@ def main():
 
     options = Options()
     options.headless = True
-    options.binary_location = '/usr/bin/firefox'
     driver = webdriver.Firefox(options=options)
     driver.maximize_window()
     scraper = TCGScraper(None, VPN, driver)
