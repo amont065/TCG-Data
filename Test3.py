dab7c4d1ef7f6f888ef14a59f7f509cfbe952e68
amont <114557081+amont065@users.noreply.github.com>
2025-08-06 21:06:02 -0700

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -10,7 +10,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 try:
     UTC = ZoneInfo("UTC")
